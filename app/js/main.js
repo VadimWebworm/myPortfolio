2573cc7dbbc671a3f9de16eab4617ce22ca9286a
VadimWebworm <Vadim-030101@yandex.ru>
2021-07-25 19:43:48 +0900

    create header

--- app/js/main.js
+++ app/js/main.js
@@ -1,32 +1,32 @@
 $(function () {
+  //preloader
+  $(window).on('load', function () {
+    $('.preloader-inner').fadeOut();
+    $('.preloader').delay(300).fadeOut('slow');
+  });
+  //logo
+  $(".screen-height").height($(window).height());
 
-  // window.onload = function () {
-  //     document.body.classList.add('loaded_hiding');
-  //     window.setTimeout(function () {
-  //       document.body.classList.add('loaded');
-  //       document.body.classList.remove('loaded_hiding');
-  //     }, 500);
-  //   }
-  //   $(window).load(function() {});
-  $(document).ready(function () {
-
+  $(window).on('resize', function () {
     $(".screen-height").height($(window).height());
-
-    $(window).resize(function () {
-      $(".screen-height").height($(window).height());
-    });
-
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
-      $('#home').css({ 'background-attachment': 'scroll' });
-    } else {
-      $('#home').parallax('50%', 0.1);
-    }
   });
 
-  $(document).ready(function ($) {
-    $(".header").menu();
+  // if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
+  //   $('#home').css({ 'background-attachment': 'scroll' });
+  // } else {
+  //   $('#home').parallax('50%', 0.1);
+  // }
+
+  //header Navbar
+  $('.header').sticky({
+    topSpacing: 0
   });
 
+  $('body').scrollspy({
+    target: '.navbar-custom',
+    offset: 70
+  })
 
+  // $(".header").menu();
 
 });
